f4b548a173731b193b68916cb72484d01d1daa86
Renan Mariano <renanbym@gmail.com>
2016-01-21 22:03:11 -0200

    init

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,42 +2,41 @@ module.exports = function(grunt) {
   grunt.initConfig({
 
     uglify : {
-     options : {
-       mangle : false
-     },
+      options : {
+        mangle : false,
+        sourceMap: true,
+        sourceMapName: 'asses/js/main.map'
+      },
 
-     my_target : {
-       files : {
-         'assets/js/main.js' : [ '_src/js/**/*' ]
-       }
-     }
-   }
+
+        files : {
+          expand: true,
+          flatten: true,
+          src: '_src/js/**/*.js',
+          dest: 'assets/js'
+          ext: '.min.js'
+        }
+    }
 
     sass : {
       dist : {
         options : { style : 'compressed' },
         files : {
-          'assets/css/style.css' : '_src/_sass/style.sass'
+          'assets/css/style.css' : '_src/sass/style.sass'
         }
       }
     },
 
     watch : {
-      dist : {
-        files : [
-          '_src/js/**/*',
-          '_src/sass/**/*'
-        ],
-
-        tasks : [ 'uglify', 'sass' ]
-      }
+      js:  { files: '_src/js/**/*.js', tasks: [ 'uglify' ] },
+      sass:  { files: 'src/sass/**/*.js', tasks: [ 'sass' ] }
     }
 
   });
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-contrib-watch' );
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.registerTask( 'default', [ 'uglify', 'sass' ] );
 
