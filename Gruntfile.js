b25b0cf7234770c164d5b32ce9090e737a0f97c3
Renan Mariano <renanbym@gmail.com>
2016-01-23 10:10:46 -0200

    .

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,29 +7,40 @@ module.exports = function(grunt) {
         sourceMap: true,
         sourceMapName: 'asses/js/main.map'
       },
-
-
-        files : {
-          expand: true,
-          flatten: true,
-          src: '_src/js/**/*.js',
-          dest: 'assets/js'
-          ext: '.min.js'
-        }
+      files : {
+        expand: true,
+        flatten: true,
+        src: '_src/js/**/*.js',
+        dest: 'assets/js',
+        ext: '.min.js'
+      }
     }
 
-    sass : {
+    ,sass : {
       dist : {
         options : { style : 'compressed' },
         files : {
           'assets/css/style.css' : '_src/sass/style.sass'
         }
       }
-    },
+    }
 
-    watch : {
+    ,imagemin: {                          // Task
+
+      dynamic: {                         // Another target
+        files: [{
+          expand: true,                  // Enable dynamic expansion
+          cwd: '_src/images',                   // Src matches are relative to this path
+          src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match
+          dest: 'assets/images'                  // Destination path prefix
+        }]
+      }
+    }
+
+    ,watch : {
       js:  { files: '_src/js/**/*.js', tasks: [ 'uglify' ] },
-      sass:  { files: 'src/sass/**/*.js', tasks: [ 'sass' ] }
+      sass:  { files: '_src/sass/**/*.js', tasks: [ 'sass' ] },
+      images:  { files: '_src/images/**/*.{png,jpg,gif}', tasks: [ 'imagemin' ] }
     }
 
   });
@@ -37,8 +48,9 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
 
-  grunt.registerTask( 'default', [ 'uglify', 'sass' ] );
+  grunt.registerTask( 'default', [ 'uglify', 'sass','imagemin'] );
 
   grunt.registerTask( 'w', [ 'watch' ] );
 
